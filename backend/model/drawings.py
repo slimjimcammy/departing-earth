07e4e67b966a5b50a00af16728fb37f74722f1f3
Cameron <yuen.cameron@gmail.com>
2024-10-05 11:15:17 -0400

    added requirements

--- backend/model/drawings.py
+++ backend/model/drawings.py
@@ -5,6 +5,6 @@ qd = QuickDrawData()
 drawingNames = qd.drawing_names
 
 for groupName in drawingNames:
-    currGroup = QuickDrawDataGroup(groupName, max_drawings=50)
+    currGroup = QuickDrawDataGroup(groupName, max_drawings=3)
     for i, drawing in enumerate(currGroup.drawings):
         drawing.image.save(f"./images/{groupName}-{i}.jpg")
