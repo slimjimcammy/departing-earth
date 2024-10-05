5cff5e63afccf819239a6c725281a653c12140bb
Cameron <yuen.cameron@gmail.com>
2024-10-05 16:13:47 -0400

    added new stardata route to app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -6,5 +7,18 @@ app = Flask(__name__)
 def test():
     return "<h1>Test successful!</h1>"
 
-# @app.route("/stardata", "POST")
-# def getStarData:
+@app.route("/stardata", methods=["GET"])
+def getStarData():
+    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,sy_dist,dec,rastr,disc_year,pl_orbper,sy_vmag,sy_kmag,st_logg+from+ps&format=json"
+    print("in stardata")
+    error = {"error": "API request failed"}
+    try:
+        response = requests.get(url)
+        code = response.status_code
+
+        return jsonify(response.json()), 200 if code == 200 else error, code
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)}), 500
+
+
+
