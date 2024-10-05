e45109cb066bb88c1cfdd89b4348982ff80b4571
agu02 <107276873+agu02@users.noreply.github.com>
2024-10-05 15:44:28 -0400

    Worm hole addition, still have to change the font

--- frontend/src/pages/Wormhole.jsx
+++ frontend/src/pages/Wormhole.jsx
@@ -5,6 +5,17 @@ const Wormhole = () => {
     return (
         <div className="WormholeContainer">
             <div className="Wormhole" />
+            <div className="Wormhole" />
+            <div className="Wormhole" />
+            <div className="Wormhole" />
+            <div className="Wormhole" />
+            <div className="Wormhole" />
+            <div className="Wormhole" />
+            <div className="Wormhole" />
+            <div className="Wormhole" /> 
+            <div className="TextOverlay">
+                <h1>Traveling through the Wormhole</h1>
+            </div>
         </div>
     );
 };
