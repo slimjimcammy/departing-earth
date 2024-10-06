7a8c86a9a76c3e8aa7bfd79629bb92987a253d4b
WillK13 <30449185+WillK13@users.noreply.github.com>
2024-10-06 18:04:12 -0400

    split up data requesting

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import json
 import requests
 from story import guessConstellation, makeStory
@@ -6,10 +7,25 @@ from transformations import transformerstar
 from PIL import Image
 
 app = Flask(__name__)
+cors = CORS(app)
 
-@app.route("/planetdata", methods=["GET"])
-def getPlanetData():
-    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,sy_dist,dec,ra,disc_year,pl_orbper,sy_vmag,sy_kmag,st_logg+from+ps&format=json"
+@app.route("/planetparams", methods=["GET"])
+@cross_origin()
+def getPlanetParams():
+    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,sy_dist,dec,ra+from+ps&format=json"
+    error = {"error": "API request failed"}
+    try:
+        response = requests.get(url)
+        code = response.status_code
+
+        return response.json() if code == 200 else None
+    except requests.exceptions.RequestException as e:
+        return None
+
+@app.route("/planetfacts", methods=["GET"])
+@cross_origin()
+def getPlanetFacts():
+    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,disc_year,pl_orbper,st_logg+from+ps&format=json"
     error = {"error": "API request failed"}
     try:
         response = requests.get(url)
@@ -20,8 +36,9 @@ def getPlanetData():
         return None
     
 @app.route("/stardata", methods=["GET"])
+@cross_origin()
 def getStarData():
-    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+hostname,ra,dec,sy_dist,sy_vmag,sy_kmag+from+stellarhosts&format=json"
+    url = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+hostname,ra,dec,sy_dist+from+stellarhosts&format=json"
     error = {"error": "API request failed"}
     try:
         response = requests.get(url)
@@ -32,12 +49,14 @@ def getStarData():
         return None
 
 @app.route("/guess", methods=["POST"])
+@cross_origin()
 def getConstellationGuess():
     payload = request.get_json()
     guess = guessConstellation(payload)
     return makeStory(guess)
 
 @app.route("/transform", methods=["POST"])
+@cross_origin()
 def changeParams():
     params = request.get_json()
     coords = []
